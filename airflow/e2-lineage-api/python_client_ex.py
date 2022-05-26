6b6c92290bd1b5fb3c67e11b3e7c322325bb1a98
Michael Robinson <merobi@gmail.com>
2022-05-26 17:07:36 -0400

    fix runid
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- airflow/e2-lineage-api/python_client_ex.py
+++ airflow/e2-lineage-api/python_client_ex.py
@@ -9,7 +9,7 @@ client = OpenLineageClient.from_environment()
 client.emit(RunEvent(
     RunState.COMPLETE,
     datetime.datetime.now().isoformat(),
-    Run(runId=uuid.UUID('123456')),
+    Run(runId='a0ccded8-dd37-11ec-9d64-0242ac120002'),
     Job(namespace=os.getenv('OPENLINEAGE_NAMESPACE'), 
     name='emit_event.wait-for-me'),
     "https://github.com/OpenLineage/OpenLineage/tree/0.0.1/integration/airflow",
